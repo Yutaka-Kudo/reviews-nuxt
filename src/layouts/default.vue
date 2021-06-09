131b168db196f9946f8bd686efccdec2452e5225
工藤泰 <yutakakudo@kudouyasushinoMacBook-Pro.local>
2021-06-10 02:07:26 +0900

    1

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -110,7 +110,7 @@ export default {
             miniVariant: false,
             right: true,
             rightDrawer: false,
-            title: "RESTAURARY",
+            title: "RESTAURary",
         };
     },
 };
