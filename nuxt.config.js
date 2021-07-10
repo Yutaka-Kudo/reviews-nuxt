43ccdf7ee2c27c72cb4e772027d6bd9a99b2108e
工藤泰 <yutakakudo@kudouyasushinoMacBook-Pro.local>
2021-07-10 20:32:38 +0900

    1

--- nuxt.config.js
+++ nuxt.config.js
@@ -239,19 +239,22 @@ export default {
 		transpile: ['gsap', 'gsap/Draggable'],
 	},
 
-	// // 使えるかわからないがpayload付き
-	// generate: {
-	// 	routes() {
-	// 		return axios.get('https://yk-restaurant-reviews-api.cyou/api/area').then(res => {
-	// 			return res.data.map(area => {
-	// 				return {
-	// 					route: `/${area.id}/ranking`,
-	// 					payload: area
-	// 				}
-	// 			})
-	// 		})
-	// 	}
-	// },
+	// 使えるかわからないがpayload付き
+	generate: {
+		async routes() {
+			let arealist = await axios.get('https://yk-restaurant-reviews-api.cyou/api/area')
+				.then(res => {
+					return res.data
+				})
+			let list = []
+			for (let areadata of arealist) {
+				let are = await axios.get(`https://yk-restaurant-reviews-api.cyou/api/stores?area=${areadata.id}`).then(function (res) { return res.data })
+				list.push({ route: `/${areadata.id}/ranking`, payload: are })
+			}
+			return list
+
+		}
+	},
 
 	// pageTransition: {
 	// 	name: 'page',
